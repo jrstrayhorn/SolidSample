24203f096765f660adf506c283bee40e49a84e31
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-07-15 13:56:29 -0400

    applying ocp refactoring moving switch to factory

---
+++ ArdalisRating/Rater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public abstract class Rater
+    {
+        protected readonly RatingEngine _engine;
+        protected readonly ConsoleLogger _logger;
+
+        public Rater(RatingEngine engine, ConsoleLogger logger)
+        {
+            _engine = engine;
+            _logger = logger;
+        }
+
+        public abstract void Rate(Policy policy);
+    }
+}
