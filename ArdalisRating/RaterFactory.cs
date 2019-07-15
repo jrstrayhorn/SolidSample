b1fad656cae8324508a3306817147e9cc043eeab
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-07-15 14:01:44 -0400

    using reflection to remove swtich in factory

--- ArdalisRating/RaterFactory.cs
+++ ArdalisRating/RaterFactory.cs
@@ -8,18 +8,31 @@ namespace ArdalisRating
     {
         public Rater Create(Policy policy, RatingEngine engine)
         {
-            switch (policy.Type)
+            // one way to do this
+            //switch (policy.Type)
+            //{
+            //    case PolicyType.Life:
+            //        return new LifePolicyRater(engine, engine.Logger);
+            //    case PolicyType.Land:
+            //        return new LandPolicyRater(engine, engine.Logger);
+            //    case PolicyType.Auto:
+            //        return new AutoPolicyRater(engine, engine.Logger);
+            //    case PolicyType.Flood:
+            //        return new FloodPolicyRater(engine, engine.Logger);
+            //    default:
+            //        return null;
+            //}
+
+            // advanced way to eliminate switch using reflection
+            try
             {
-                case PolicyType.Life:
-                    return new LifePolicyRater(engine, engine.Logger);
-                case PolicyType.Land:
-                    return new LandPolicyRater(engine, engine.Logger);
-                case PolicyType.Auto:
-                    return new AutoPolicyRater(engine, engine.Logger);
-                case PolicyType.Flood:
-                    return new FloodPolicyRater(engine, engine.Logger);
-                default:
-                    return null;
+                return  (Rater)Activator.CreateInstance(
+                    Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"),
+                    new object[] { engine, engine.Logger });
+            }
+            catch
+            {
+                return null;
             }
         }
     }
